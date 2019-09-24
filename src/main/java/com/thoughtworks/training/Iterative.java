16dd040149edc3f47893879c40b99f16fadc2cbc
dipakkailaspatil <patil.dipak363@gmail.com>
2019-09-24 11:36:20 +0530

    [Dipak|Dipali] Add. Iterative by Passing two char Array

--- src/main/java/com/thoughtworks/training/Iterative.java
+++ src/main/java/com/thoughtworks/training/Iterative.java
@@ -1,4 +1,35 @@
 package com.thoughtworks.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Iterative {
+    char[] charArray;
+    char[] newCharArray;
+    List<String> list=new ArrayList<String>();
+    public Iterative(char[] charArray) {
+        this.charArray=charArray;
+    }
+
+    public List<String> arrange() {
+        newCharArray=charArray;
+        for(int index=0;index<newCharArray.length;index++) {
+            swap(index);
+        }
+        return list;
+    }
+
+    void swap(int index) {
+        for(int j=0;j<newCharArray.length;j++) {
+            if(index!=j) {
+                char temp=newCharArray[index];
+                newCharArray[index]=newCharArray[j];
+                newCharArray[j]=temp;
+
+                String str=String.valueOf(newCharArray);
+                System.out.println(str);
+                list.add(str);
+            }
+        }
+    }
 }
--- src/test/java/com/thoughtworks/training/IterativeTest.java
+++ src/test/java/com/thoughtworks/training/IterativeTest.java
@@ -1,4 +1,31 @@
 package com.thoughtworks.training;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IterativeTest {
+
+    @Test
+    void givenSingleCharArray_WhenPermute_ThenReturnList() {
+        List<String> l=new ArrayList<String>();
+        l.add("a");
+        char[] charArray={'a'};
+        Iterative iterative=new Iterative(charArray);
+        Assertions.assertEquals(l,iterative.arrange());
+    }
+
+    @Test
+    void givenTwoCharArray_WhenPermute_ThenReturnList() {
+        List<String> l=new ArrayList<String>();
+        l.add("ab");
+        l.add("ba");
+        char[] charArray={'a','b'};
+        Iterative iterative=new Iterative(charArray);
+        Assertions.assertEquals(l,iterative.arrange());
+    }
+
 }
